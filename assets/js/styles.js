85472a830cc3154fc7838d0fe7cbc7b2638b939c
wade <b0231046@ems.niu.edu.tw>
2018-11-03 19:11:11 +0800

    coby V2_4

---
+++ assets/js/styles.js
@@ -0,0 +1,19 @@
+// var x = document.getElementsById("buyWT");
+var x = $("#buyWT").val()
+
+// $("#buyWT").click(function() {
+//     console.log($(this).val());
+//     $(":text").val($(this).val());
+// });
+
+// console.log(1 == 1);
+console.log(x);
+
+// document.getElementById("demo").innerHTML = x[1].innerHTML;
+// document.getElementById("demo").innerHTML = 55555;
+
+$("#definite").click(
+    function() {
+        $(":text").val($("#buyWT").val() + $("#span").val() + $("#note").val());
+    }
+)
